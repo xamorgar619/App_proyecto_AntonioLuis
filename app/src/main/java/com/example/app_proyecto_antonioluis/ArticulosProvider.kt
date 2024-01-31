cb0f6249c322d179178ad9643a645e1338af476c
Antonio Luis Morales García <xamorgar619@ieshnosmachado.org>
2024-01-31 18:20:38 +0100

    .

---
+++ app/src/main/java/com/example/app_proyecto_antonioluis/ArticulosProvider.kt
@@ -0,0 +1,17 @@
+package com.example.app_proyecto_antonioluis
+
+class ArticulosProvider {
+
+    companion object {
+        val articulos = listOf<Articulo>(
+            Articulo(
+                "ASUS ZenBook 14 OLED",
+                "999,00 €",
+                "Procesador Intel® Core™ i7-1360P, 16 GB de RAM, 512 GB SSD",
+                "https://img.pccomponentes.com/articles/1077/10771401/1552-asus-zenbook-14-oled-ux3402va-km209w-intel-evo-core-i7-1360p-16gb-512gb-ssd-14.jpg"
+            ),
+
+        )
+    }
+
+}
