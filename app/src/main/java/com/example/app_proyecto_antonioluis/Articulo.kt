002cb0b79d71b165b636d87dcd679cd07e7df34b
Antonio Luis Morales García <xamorgar619@ieshnosmachado.org>
2024-01-31 17:58:54 +0100

    .

---
+++ app/src/main/java/com/example/app_proyecto_antonioluis/Articulo.kt
@@ -0,0 +1,11 @@
+package com.example.app_proyecto_antonioluis
+
+data class Articulo (
+
+    val nombre: String,
+    val precio: String,
+    val descripcion: String,
+    val imagenUrl: String
+)
+
+
--- app/src/main/java/com/example/app_proyecto_antonioluis/ArticulosFragment.kt
+++ app/src/main/java/com/example/app_proyecto_antonioluis/ArticulosFragment.kt
@@ -8,26 +8,21 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class CochesFragment : Fragment(R.layout.fragment_coches) {
-
-    val imagenes = listOf(R.drawable.coche1, R.drawable.coche2, R.drawable.coche3, R.drawable.coche4,
-        R.drawable.coche5, R.drawable.coche6, R.drawable.coche7)
-    val titulos = listOf("Ferrari 812 GTS", "McLaren 720S", "Mercedes EQS Eléctrico", "Mercedes Clase S",
-        "Rolls-Royce Wraith", "Bentley Continental GTC", "Aston Martin DBX")
+class ArticulosFragment : Fragment(R.layout.fragment_articulos) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.rvCoches)
+        val recyclerView: RecyclerView = view.findViewById(R.id.rvArticulos)
         recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
-        val adapter = ImagenAdapter(imagenes, titulos)
+        val articulos = ArticulosProvider.articulos
+
+        val adapter = ArticuloAdapter(articulos)
         recyclerView.adapter = adapter
 
         val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
-            findNavController().navigate(R.id.action_CochesFragment_to_inicioFragment)
+            findNavController().navigate(R.id.action_ArticulosFragment_to_inicioFragment)
         }
-
     }
-
-    }
+}
